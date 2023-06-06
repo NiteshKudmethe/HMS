a1b767c71ec91c372ebe23c1adaa4e2a66e6ab4d
nitesh kudmethe <niteshkudmethe@niteshs-MacBook-Pro.local>
2023-06-06 14:26:06 +0530

    Your commit message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+import { LandingComponent } from './landing/landing.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { DoctorDashboardComponent } from './doctor-dashboard/doctor-dashboard.component';
+import { PatientDashboardComponent } from './patient-dashboard/patient-dashboard.component';
+import { AppointmentBookingComponent } from './appointment-booking/appointment-booking.component';
+import { PrescriptionFormComponent } from './prescription-form/prescription-form.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    LoginComponent,
+    LandingComponent,
+    DoctorDashboardComponent,
+    PatientDashboardComponent,
+    AppointmentBookingComponent,
+    PrescriptionFormComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BsDatepickerModule.forRoot(),
+    BrowserAnimationsModule // or NoopAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
